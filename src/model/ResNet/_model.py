f0f132d3d18cd22509a43005ef09a2273d1edcb2
Paul Jeha <paul.jeha@gmail.com>
2021-11-27 12:34:40 +0100

    Doc in _model and .gitgnore

--- src/model/ResNet/_model.py
+++ src/model/ResNet/_model.py
@@ -3,11 +3,28 @@ import torch.nn as nn
 
 
 class VisionNetwork(nn.Module):
+    """Base class for the computer vision network we will use.
+
+    This class inherits from nn.Module. Its purpose is to implement the common functions
+    used in all the computer vision network we will use.
+    """
+
     def __init__(self) -> None:
         super(VisionNetwork, self).__init__()
         pass
 
     def _flatten(self, image: torch.Tensor) -> torch.Tensor:
+        """This function flatten an input tensor.
+
+        Takes as input a tensor of shape (B, C1,...,Cn) and returns a tensor of
+        shape (B, C1 * ... * Cn).
+
+        Args:
+            image: a torch.tensor of shape (B, C1,...,Cn)
+        Returns:
+            tensor of shape (B, C1 * ... * Cn)
+        """
+
         pass
 
 
